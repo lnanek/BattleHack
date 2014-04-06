2ab81b5699c0381deeaf78b9dc4e636657d9fe81
Lance Nanek <lnanek@gmail.com>
2014-04-05 22:22:18 -0700

    Select recipient in Android app and trigger complete payment POST request to server.

--- PayBeamGlassware/src/name/nanek/paybeam/MakePaymentActivity.java
+++ PayBeamGlassware/src/name/nanek/paybeam/MakePaymentActivity.java
@@ -14,145 +14,200 @@ import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.os.StrictMode;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 public class MakePaymentActivity extends Activity {
 	
-	private static final String LOG_TAG = "MakePaymentActivity";
+	private static final int MSG_ID_GOT_RECIPIENTS = 1;
 	
-	private static final String SERVER = "http://10.0.2.2:9091/adaptivepaymentssample/BattleHackReadDatabase";
+	private static final int MSG_ID_ERROR = 2;
 
+	private static final String LOG_TAG = "MakePaymentActivity";
+
+	private static final String SERVER = "http://10.0.2.2:9091/adaptivepaymentssample/";
+
+	private static final String DOWNLOAD_URL = SERVER + "BattleHackReadDatabase";
+	
+	private static final String SUBMIT_URL = SERVER + "BattleHackCompletePayment";
+	
 	private TextView text;
-	
+
+	private Handler handler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			recipients = (List<Recipient>) msg.obj;
+			progress.hide();
+			
+			text.setText(recipients.toString());
+
+			openOptionsMenu();
+		}
+	};
+
 	private List<Recipient> recipients = new LinkedList<Recipient>();
-	
+
+	private ProgressDialog progress;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		
+
 		Log.i(LOG_TAG, "onCreate");
-		
+
 		super.onCreate(savedInstanceState);
-		
+
 		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 
-		StrictMode.setThreadPolicy(policy); 
+		StrictMode.setThreadPolicy(policy);
 
 		setContentView(R.layout.activity_make_payment);
 
 		text = (TextView) findViewById(R.id.text);
-		
-		
-		
-		// TODO show downloading prompt
-		
-		
-		// TODO do on other thread
-		readReceipients();
-				
-		// TODO pick person by swiping
-				
-		// TODO make payment		
-		
-		
+		text.setText("Downloading...");
+
+		progress = new ProgressDialog(this);
+		progress.show();
+
+		final Thread thread = new Thread() {
+			@Override
+			public void run() {
+				readReceipients();
+			}
+		};
+		thread.start();
 	}
-	
-	public String readReceipients() {
-		
+
+	public void readReceipients() {
+
 		Log.i(LOG_TAG, "readReceipients");
 
-	    StringBuilder builder = new StringBuilder();
-	    HttpClient client = new DefaultHttpClient();
-	    HttpGet httpGet = new HttpGet(SERVER);
-	    try {
-	      HttpResponse response = client.execute(httpGet);
-	      StatusLine statusLine = response.getStatusLine();
-	      int statusCode = statusLine.getStatusCode();
+		HttpClient client = new DefaultHttpClient();
+		HttpGet httpGet = new HttpGet(DOWNLOAD_URL);
+		try {
+			HttpResponse response = client.execute(httpGet);
+			StatusLine statusLine = response.getStatusLine();
+			int statusCode = statusLine.getStatusCode();
 
-	      
-	      if (statusCode == 200) {
-	        HttpEntity entity = response.getEntity();
-	        InputStream content = entity.getContent();
-	        BufferedReader reader = new BufferedReader(new InputStreamReader(content));
-	        String line;
-	        while ((line = reader.readLine()) != null) {
-	          builder.append(line);
-	        }
-	        
-	        text.setText(builder);
-	        
-	        
-	        
-	        Gson gson = new Gson();
-	        Type collectionType = new TypeToken<List<Recipient>>(){}.getType();
-	        recipients = gson.fromJson(builder.toString(), collectionType);
-	        
-            Log.d("tag", "List: " + recipients);
+			if (statusCode == 200) {
+				HttpEntity entity = response.getEntity();
+				InputStream content = entity.getContent();
+				BufferedReader reader = new BufferedReader(new InputStreamReader(content));
 
-            // TODO
-          //openOptionsMenu();       
-	        
-	      } else {
-	        text.setText("Failed to download file");
-	      }
-	    } catch (ClientProtocolException e) {
-	      e.printStackTrace();
-	      text.setText("Failed to download file: " + e.getClass().getName());
-	    } catch (IOException e) {
-	      e.printStackTrace();
-	      text.setText("Failed to download file: " + e.getClass().getName());
-	    }
-	    
-	    return builder.toString();
-	  }
-	
-	
-	// TODO offer menu items parsed from download
-	
-    private static final int UPDATE_DATA = 0;
-    private static final int ADD_NEW = 1;
+				StringBuilder builder = new StringBuilder();				
+				String line;
+				while ((line = reader.readLine()) != null) {
+					builder.append(line);
+				}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+				Gson gson = new Gson();
+				Type collectionType = new TypeToken<List<Recipient>>() {
+				}.getType();				
+				final List<Recipient> recipients = gson.fromJson(builder.toString(), collectionType);
+				Log.d(LOG_TAG, "List: " + recipients);
+
+				final Message message = handler.obtainMessage(MSG_ID_GOT_RECIPIENTS, recipients);
+				handler.sendMessage(message);
+
+
+			} else {
+				final Message message = handler.obtainMessage(MSG_ID_ERROR, "Failed to download file");
+				handler.sendMessage(message);
+			}
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+			final Message message = handler.obtainMessage(MSG_ID_ERROR, "Failed to download file: " + e.getClass().getName());
+			handler.sendMessage(message);
+		} catch (IOException e) {
+			e.printStackTrace();
+			final Message message = handler.obtainMessage(MSG_ID_ERROR, "Failed to download file: " + e.getClass().getName());
+			handler.sendMessage(message);
+		}
+	}
+	
+	public void completePayment(final String recipient, final String username) {
+
+		Log.i(LOG_TAG, "completePayment");
+
+		// TODO send DB index for recipient,
+		// google auth token for user
+		// for better security
+		HttpClient client = new DefaultHttpClient();
+		HttpPost httpPost = new HttpPost(SUBMIT_URL
+				+ "?recipient=" + recipient
+				+ "&username=" + username);
+		try {
+			HttpResponse response = client.execute(httpPost);
+			StatusLine statusLine = response.getStatusLine();
+			int statusCode = statusLine.getStatusCode();
+
+			if (statusCode == 200) {
+
+				final Message message = handler.obtainMessage(MSG_ID_ERROR, "Payment complete");
+				handler.sendMessage(message);
+
+
+			} else {
+				final Message message = handler.obtainMessage(MSG_ID_ERROR, "Failed to complete");
+				handler.sendMessage(message);
+			}
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+			final Message message = handler.obtainMessage(MSG_ID_ERROR, "Failed to complete: " + e.getClass().getName());
+			handler.sendMessage(message);
+		} catch (IOException e) {
+			e.printStackTrace();
+			final Message message = handler.obtainMessage(MSG_ID_ERROR, "Failed to complete: " + e.getClass().getName());
+			handler.sendMessage(message);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
 
 		Log.i(LOG_TAG, "onCreateOptionsMenu");
+		
+		for(int index = 0; index < recipients.size(); index++) {
+			final Recipient recipient = recipients.get(index);
 
-        menu.add(0,UPDATE_DATA,0,"Update Information");
+			final int menuItemId = index;
+			menu.add(0, menuItemId, 0, recipient.email + " - $" + recipient.amount);						
+		}
 
-        menu.add(0,ADD_NEW,1,"Add New Inspection"); 
-        
-        return true;
-    }
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
 
 		Log.i(LOG_TAG, "onOptionsItemSelected");
-
 		
-        switch(item.getItemId())
-        {
-           case(UPDATE_DATA):
-               Log.d("tag", "update");
-               break;
-           case(ADD_NEW):
-              Log.d("tag", "Add");
-               break; 
-          } 
-        return false; 
-        
-        
-        
-    }
+		final Recipient recipient = recipients.get(item.getItemId());
+		text.setText("Selected = " + recipient.email);
+		
+		final Thread thread = new Thread() {
+			@Override
+			public void run() {
+				// TODO lookup email from Google Glass account
+				completePayment(recipient.email, "lnanek@gmail.com");
+			}
+		};
+		thread.start();
+		
+		return false;
+	}
 
 }
---
+++ adaptivepayments-sdk-java/adaptivepaymentssample/src/main/java/com/sample/adaptivepayments/BattleHackCompletePayment.java
@@ -0,0 +1,74 @@
+package com.sample.adaptivepayments;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.paypal.exception.ClientActionRequiredException;
+import com.paypal.exception.HttpErrorException;
+import com.paypal.exception.InvalidCredentialException;
+import com.paypal.exception.InvalidResponseDataException;
+import com.paypal.exception.MissingCredentialException;
+import com.paypal.exception.SSLConfigurationException;
+import com.paypal.sdk.exceptions.OAuthException;
+import com.paypal.svcs.services.AdaptivePaymentsService;
+import com.paypal.svcs.types.ap.ConvertCurrencyRequest;
+import com.paypal.svcs.types.ap.ConvertCurrencyResponse;
+import com.paypal.svcs.types.ap.CurrencyCodeList;
+import com.paypal.svcs.types.ap.CurrencyConversionList;
+import com.paypal.svcs.types.ap.CurrencyList;
+import com.paypal.svcs.types.common.CurrencyType;
+import com.paypal.svcs.types.common.RequestEnvelope;
+import com.sample.util.Configuration;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BattleHackCompletePayment extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public BattleHackCompletePayment() {
+		super();
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		final String recipient = request.getParameter("recipient");
+		final String username = request.getParameter("username");
+				
+        response.setContentType("text/plain");
+        PrintWriter out = response.getWriter();
+        out.println("Completing payment...");
+
+
+        // TODO lookup preapproval key, make payment using key
+
+
+        out.println("Done.");
+        out.close();
+
+
+	}
+
+}
